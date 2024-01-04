596aacc4f513cd6052309d4218ea02c074fbf00d
Oleh Lunev <129771696+OlehLunov@users.noreply.github.com>
2024-01-04 18:13:12 +0200

    some done

--- birthday/src/components/list/List.jsx
+++ birthday/src/components/list/List.jsx
@@ -1,28 +1,20 @@
 import React from 'react'
 import "./List.css"
 
-export default function List({info}) {
-  return (
-        <ul>
-            {iterate(info)}
-        </ul>
-  )
-}
-
-
 function iterate(data){
     if (!data) return;
     return(
         <>
             {
                 data.map((person, index) => {
+           
                     return (
                         <li key={index}>
                             <div className="flex">
                                 <img src={person.img} alt="img" />
                                 <div className="title">
                                     <h3 className='name'>{person.name}</h3>
-                                    <h5 className='age'>{person.birthday}</h5>
+                                    <h5 className='age'>{Old(person.birthday)} years</h5>
                                 </div>
                             </div>
                         </li>
@@ -32,3 +24,25 @@ function iterate(data){
         </>
     )
 }
+
+
+function Old(personAge){
+    let year = new Date(personAge).getFullYear()
+    let currentYear =  new Date().getFullYear();
+    let age = currentYear - year;
+
+    return age;
+}
+
+
+
+
+
+export default function List({info}) {
+  return (
+        <ul>
+            {iterate(info)}
+        </ul>
+  )
+}
+
